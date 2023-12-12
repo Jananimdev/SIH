61488bbfeae81c8814d6ec543e49dc1f86c61751
RC Balaji <121729091+rc-balaji@users.noreply.github.com>
2023-12-12 16:24:43 +0530

    Update Model.js

--- Server/Model/Model.js
+++ Server/Model/Model.js
@@ -7,7 +7,7 @@ const userSchema = new mongoose.Schema({
   password: String,
   phno: String,
   aadharNumber: String,
-  isAdmin:Boolean,
+  isAdmin:String,
 });
 
 const User = mongoose.model('User', userSchema);
