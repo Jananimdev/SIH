c69b2ce756c626ae439a7ff1ad82d117429e3f90
rc-balaji <balajic@drngpit.ac.in>
2023-12-20 00:48:22 +0530

    ssss

--- Server/Router/router.js
+++ Server/Router/router.js
@@ -3,7 +3,31 @@ const express = require('express');
 const User = require('../Model/Model');
 
 const router = express.Router();
+const mqtt = require('mqtt');
 
+const mqttBrokerUrl = 'mqtt://broker.hivemq.com';
+const mqttTopic = 'kt-datavim/7.9';
+const mqttQoS = 2;
+const mqttClient = mqtt.connect(mqttBrokerUrl);
+
+mqttClient.on('connect', () => {
+  console.log('Connected to MQTT broker');
+});
+router.post('/receiveData', (req, res) => {
+  const dataFromReact = req.body;
+
+  // Publish the received data to MQTT
+  const message = JSON.stringify(dataFromReact);
+  mqttClient.publish(mqttTopic, message, { qos: mqttQoS }, (err) => {
+    if (!err) {
+      console.log(`Published message to ${mqttTopic}: ${message}`);
+      res.status(200).send('Data received and published to MQTT');
+    } else {
+      console.error('Error publishing message to MQTT:', err);
+      res.status(500).send('Internal Server Error');
+    }
+  });
+});
 router.get('/users', async (req, res) => {
   try {
     const users = await User.find();
